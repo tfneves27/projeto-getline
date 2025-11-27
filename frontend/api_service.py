b8f58769680f43783e14bee6870b61754ffa3122
Tiago Félix Neves <tiagoneves270105@gmail.com>
2025-11-27 19:45:52 -0300

    Atualizando produtos

--- frontend/api_service.py
+++ frontend/api_service.py
@@ -3,15 +3,17 @@ import httpx
 class ApiService:
     async def buscar_produtos_destaque(self, termo=None):
         async with httpx.AsyncClient() as client:
+            base_url = "https://getline-api.onrender.com"
             if termo:
-                url = f"http://127.0.0.1:8000/produtos/busca?termo={termo}"
+                url = f"{base_url}/produtos/busca?termo={termo}"
             else:
-                url = "http://127.0.0.1:8000/produtos"
+                url = f"{base_url}/produtos"
             
             response = await client.get(url)
             return response.json()
         
     async def buscar_promocoes(self):
         async with httpx.AsyncClient() as client:
-            response = await client.get("http://127.0.0.1:8000/promocoes")
+            base_url = "https://getline-api.onrender.com"
+            response = await client.get(f"{base_url}/promocoes")
             return response.json()
